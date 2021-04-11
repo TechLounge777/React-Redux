c1325cf07291b64138a9e9700928cbd6cfd25626
TechLounge777 <msblanche777@gmail.com>
2021-04-11 11:10:03 +0200

    added reducer

--- src/redux/cake/cakeReducer.js
+++ src/redux/cake/cakeReducer.js
@@ -1,3 +1,17 @@
-import React from 'react'
+import { BUY_CAKE } from './cakeTypes'
 
+const initialState = {
+    numOfCakes: 10
+}
 
+const cakeReducer = (state = initialState, action) => {
+    switch(action.type) {
+    case BUY_CAKE: return {
+        ...state, 
+        numOfcakes: state.numOfCakes -1 
+    }
+    default: return state        
+    }
+}
+
+export default cakeReducer
